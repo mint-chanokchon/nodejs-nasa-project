2ef25bd97c6d0c2c73cc5987fa0314c4eacf8a0c
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-03-17 09:47:44 +0700

    add cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
+const whitelist = ['http://localhost:3000'];
+app.use(cors({
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+}));
 app.use(express.json()); // แปรงข้อมูลใน body เป็น json ให้
 
 // End point
