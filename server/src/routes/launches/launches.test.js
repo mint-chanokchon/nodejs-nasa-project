44f6a161942ac04e9ac52f0f39056f4c31424b06
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2023-04-15 08:01:40 +0700

    add jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,20 @@
+describe('Test GET /launches', () => {
+    test('Should response with 200 success', () => {
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+describe('Test POST /launches', () => {
+    test('Should response with 200 success', () => {
+
+    });
+
+    test('Should catch missing required properties', () => {
+
+    });
+
+    test('Should catch invalid dates', () => {
+
+    });
+});
